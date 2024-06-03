92683e4ebf09b15a44adfb035ab782770300550f
cylynnx <winstoneser@gmail.com>
2024-06-03 18:41:37 -0400

    changing towers

--- scripts/tower.gd
+++ scripts/tower.gd
@@ -1,11 +1,23 @@
 extends Node2D
 
 var tower_textures: Array[String] = [
-	"res://assets/test/towerline001.png",
-	"res://assets/test/towerline002.png",
-	"res://assets/test/towerline003.png"
+	"res://assets/WallTower/BlueSlices/Slice001.png",
+	"res://assets/WallTower/BlueSlices/Slice002.png",
+	"res://assets/WallTower/BlueSlices/Slice003.png",
+	"res://assets/WallTower/BlueSlices/Slice004.png",
+	"res://assets/WallTower/BlueSlices/Slice005.png",
+	"res://assets/WallTower/BlueSlices/Slice006.png",
+	"res://assets/WallTower/BlueSlices/Slice007.png",
+	"res://assets/WallTower/BlueSlices/Slice008.png",
+	"res://assets/WallTower/BlueSlices/Slice009.png",
+	"res://assets/WallTower/BlueSlices/Slice010.png",
+	"res://assets/WallTower/BlueSlices/Slice011.png",
+	"res://assets/WallTower/BlueSlices/Slice012.png",
+	"res://assets/WallTower/BlueSlices/Slice013.png",
+	"res://assets/WallTower/BlueSlices/Slice014.png",
+	"res://assets/WallTower/BlueSlices/Slice015.png"
 	]
 	
 func _ready():
-	$Texture.scale = Vector2(0.15, 0.25)
+	$Texture.scale = Vector2(0.30, 0.35)
 	$Texture.texture = load(tower_textures.pick_random())
