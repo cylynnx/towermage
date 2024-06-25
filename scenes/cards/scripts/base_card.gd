0d6fd8882eff351b3519ca8227486d0a9f431903
cylynnx <winstoneser@gmail.com>
2024-06-25 16:18:49 -0400

    nice commit

--- scenes/cards/scripts/base_card.gd
+++ scenes/cards/scripts/base_card.gd
@@ -1,7 +1,30 @@
-extends Node2D
-class_name BaseCard
+class_name BaseCard extends Node2D
 
+enum CardType {RESOURCE, MAGIC, CREATURE}
+
+const RESOURCE_COLOR: Color = Color(0.853, 0.344, 0)
+const MAGIC_COLOR: Color = Color(0.137, 0.447, 0.78)
+const CREATURE_COLOR: Color = Color(0.89, 0.58, 0.878)
+
+@export var description: String
+@export var card_type: CardType
+@export var card_cost: int
+
+func _ready():
+	match card_type:
+		CardType.RESOURCE:
+			$Cost.font_color = RESOURCE_COLOR
+		CardType.MAGIC:
+			$Cost.font_color = MAGIC_COLOR
+		CardType.CREATURE:
+			$Cost.font_color = CREATURE_COLOR
+		_:
+			$Cost.font_color = Color(0, 0, 0, 1)
+			
+	$Cost.text = str(card_cost)
+	
 func _on_area_2d_mouse_entered():
+	$Sound/SoundPlayer.play()
 	var tween = create_tween()
 	tween.tween_property(self, "scale", Vector2(0.36, 0.36), 0.2)
 	
---
+++ scripts/cards.gd
@@ -0,0 +1,3 @@
+extends Node
+
+const Agriculture = preload("res://scenes/cards/Agriculture.tscn")
