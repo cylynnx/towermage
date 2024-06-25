b50b9a54e371a7edd98b510a8c730e9ce38cdd51
cylynnx <winstoneser@gmail.com>
2024-06-25 18:55:04 -0400

    nice comit

--- scenes/cards/scripts/base_card.gd
+++ scenes/cards/scripts/base_card.gd
@@ -2,18 +2,9 @@ class_name BaseCard extends Node2D
 
 enum CardType {RESOURCE, MAGIC, CREATURE}
 
-const RESOURCE_COLOR: Color = Color(0.853, 0.344, 0)
-const MAGIC_COLOR: Color = Color(0.137, 0.447, 0.78)
-const CREATURE_COLOR: Color = Color(0.89, 0.58, 0.878)
-
 @export var description: String
 @export var card_type: CardType
 @export var card_cost: int
-
-func _ready():
-	print(RESOURCE_COLOR.to_html())
-	print(MAGIC_COLOR.to_html())
-	print(CREATURE_COLOR.to_html())
 	
 func _on_area_2d_mouse_entered():
 	$Sound/SoundPlayer.play()
--- scripts/game.gd
+++ scripts/game.gd
@@ -2,4 +2,5 @@ class_name Game extends Node2D
 
 func _ready():
 	var card: BaseCard = Cards.Agriculture.instantiate() as BaseCard
+	card.position = Vector2(200.0, 300.0)
 	add_child(card)
